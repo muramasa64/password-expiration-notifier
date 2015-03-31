8f92b2aac3cc3bef97ee81f792225491345b3ccd
ISOBE Kazuhiko <muramasa64@gmail.com>
2015-03-31 14:07:54 +0900

    format source

--- lib/password_expiration_notifier/cli.rb
+++ lib/password_expiration_notifier/cli.rb
@@ -5,22 +5,49 @@ require 'thor'
 module PasswordExpirationNotifier
   class CLI < Thor
     include Utils
-    class_option :config, aliases: [:c], type: :string,  desc: "use config file", default: "notifier.config"
-    class_option :debug,                 type: :boolean, desc: "debug mode"
+    class_option :config,
+      aliases: [:c],
+      type: :string,
+      desc: "use config file",
+      default: "notifier.config"
+    class_option :debug,
+      type: :boolean,
+      desc: "debug mode"
 
     desc "list [--all] [--expire-within=DAYS]", "show account list of Active Directory"
-    option :all,             aliases: [:a], type: :boolean, desc: "show all accounts"
-    option :expire_within,   aliases: [:e], type: :numeric, desc: "expire within n days", default: 7
-    option :expiration_days, aliases: [:d], type: :numeric, desc: "password expiration days", default: 90
+    option :all,
+      aliases: [:a],
+      type: :boolean,
+      desc: "show all accounts"
+    option :expire_within,
+      aliases: [:e],
+      type: :numeric,
+      desc: "expire within n days",
+      default: 7
+    option :expiration_days,
+      aliases: [:d],
+      type: :numeric,
+      desc: "password expiration days",
+      default: 90
     def list()
       users = fetch_users(config(options))
       show_list(users)
     end
 
     desc "notify [--expire-within=DAYS] [--dry-run]", "send notify mail to the account it expires"
-    option :expire_within,   aliases: [:e], type: :numeric, desc: "expire within n days", default: 7
-    option :expiration_days, aliases: [:d], type: :numeric, desc: "password expiration days", default: 90
-    option :dry_run,                        type: :boolean, desc: "show list only. do not sent notify"
+    option :expire_within,
+      aliases: [:e],
+      type: :numeric,
+      desc: "expire within n days",
+      default: 7
+    option :expiration_days,
+      aliases: [:d],
+      type: :numeric,
+      desc: "password expiration days",
+      default: 90
+    option :dry_run,
+      type: :boolean,
+      desc: "show list only. do not sent notify"
     def notify()
       conf = config(options)
       users = fetch_users(conf)
